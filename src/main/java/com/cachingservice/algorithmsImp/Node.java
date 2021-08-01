8cc4a86811f71072225384174169fe6b49019ec4
dshahid380 <dshahid3805@gmail.com>
2021-08-01 17:31:47 +0530

    Initial Commit.

---
+++ src/main/java/com/cachingservice/algorithmsImp/Node.java
@@ -0,0 +1,12 @@
+package com.cachingservice.algorithmsImp;
+
+public class Node {
+    private Node left;
+    private Node right;
+    private String key;
+    public Node(String key) {
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
