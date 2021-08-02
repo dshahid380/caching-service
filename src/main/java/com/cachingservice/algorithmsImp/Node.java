6111f414174e6ad5ca1038521d0fd0ebd6958b50
dshahid380 <dshahid3805@gmail.com>
2021-08-02 13:04:53 +0530

    Added common functionalities of a node.

--- src/main/java/com/cachingservice/algorithmsImp/Node.java
+++ src/main/java/com/cachingservice/algorithmsImp/Node.java
@@ -4,9 +4,27 @@ public class Node {
     private Node left;
     private Node right;
     private String key;
-    public Node(String key) {
+    private String value;
+    public Node(String key, String value) {
         this.key = key;
+        this.value = value;
         this.left = null;
         this.right = null;
+    }
+
+    public Node getLeftNodeRef() {
+        return left;
+    }
+
+    public Node getRightNodeRef() {
+        return right;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
     }
 }
