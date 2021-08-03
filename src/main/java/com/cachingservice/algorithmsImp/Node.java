daae3c18beae9f1e69586d37896b2f3e88493a43
Md Shahid <dshahid3805@gmail.com>
2021-08-03 08:09:07 +0530

    Dev (#3)
    * Added level counter and setter methods for rest of attributes.

--- src/main/java/com/cachingservice/algorithmsImp/Node.java
+++ src/main/java/com/cachingservice/algorithmsImp/Node.java
@@ -1,19 +1,28 @@
 package com.cachingservice.algorithmsImp;
 
+import java.util.Objects;
+
 public class Node {
     private Node left;
     private Node right;
     private String key;
     private String value;
+    private int cnt;
+
     public Node(String key, String value) {
         this.key = key;
         this.value = value;
         this.left = null;
         this.right = null;
+        this.cnt = 1;
     }
 
     public Node getLeftNodeRef() {
         return left;
+    }
+
+    public void incrementCnt() {
+        this.cnt++;
     }
 
     public Node getRightNodeRef() {
@@ -26,5 +35,41 @@ public class Node {
 
     public String getValue() {
         return value;
+    }
+
+    public int getCnt() {
+        return cnt;
+    }
+
+    public void setLeftNodeRef(Node left) {
+        this.left = left;
+    }
+
+    public void setRightNodeRef(Node right) {
+        this.right = right;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Node)) return false;
+        Node node = (Node) o;
+        return Objects.equals(getLeftNodeRef(), node.getLeftNodeRef())
+                && Objects.equals(getRightNodeRef(), node.getRightNodeRef())
+                && Objects.equals(getKey(), node.getKey())
+                && Objects.equals(getValue(), node.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLeftNodeRef(), getRightNodeRef(), getKey(), getValue());
     }
 }
