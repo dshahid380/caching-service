45d301c62b471489d49bf87cdffdb33dc1a3cf66
dshahid380 <dshahid3805@gmail.com>
2022-02-13 23:34:16 +0530

    Add controller for insert operation

---
+++ src/main/java/com/cachingservice/controller/CacheController.java
@@ -0,0 +1,32 @@
+package com.cachingservice.controller;
+
+import com.cachingservice.dao.CacheDAO;
+import com.cachingservice.model.KeyValue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class CacheController {
+    Logger log = LoggerFactory.getLogger(CacheController.class);
+
+    // TODO: Validate request body and raise respective exception
+    @PostMapping(value = "/insert", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> cacheElement(@RequestBody KeyValue keyValue) {
+        try {
+            //TODO: Implement business logic here
+
+            return new ResponseEntity<>("Successfully inserted the key-value pair", HttpStatus.OK);
+        } catch (Exception exception) {
+            log.error("Failed to insert the key-value pair: ", exception);
+            return new ResponseEntity<>("Failed to insert", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+}
